bfe2175769ed16a92bf99f8ddf97f795369ff211
Matt Taila <mtailacodes@gmail.com>
2019-11-20 03:32:32 +0000

    Setup dependency graph for modules

--- app/src/main/java/com/example/revolut/RevolutApp.kt
+++ app/src/main/java/com/example/revolut/RevolutApp.kt
@@ -1,11 +1,28 @@
 package com.example.revolut
 
 import android.app.Application
+import com.example.revolut.core.coreModule
+import com.example.revolut.currencyrate.di.currencyRateModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class RevolutApp : Application(){
 
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidLogger(Level.INFO)
+            androidContext(this@RevolutApp)
+            modules(
+                listOf(
+                    coreModule,
+                    currencyRateModule
+                )
+            )
+        }
+
     }
 
 }
