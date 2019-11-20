756af9f5eb93cf830021b67bce183b8390d8b778
Matt Taila <mtailacodes@gmail.com>
2019-11-20 03:35:22 +0000

    Added HttpLoggingInterceptor

--- app/src/main/java/com/example/revolut/core/CoreModule.kt
+++ app/src/main/java/com/example/revolut/core/CoreModule.kt
@@ -6,6 +6,7 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 const val baseUrl = "https://revolut.duckdns.org/latest"
@@ -24,6 +25,7 @@ val coreModule = module {
     single {
         OkHttpClient
             .Builder()
+            .addInterceptor(HttpLoggingInterceptor())
             .build()
     }
 
