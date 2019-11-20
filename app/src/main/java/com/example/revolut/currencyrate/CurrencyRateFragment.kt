88143b102f016b4feaf2a17fa2e6682a968f6383
Matt Taila <mtailacodes@gmail.com>
2019-11-20 02:42:46 +0000

    Added business logic and stub data for epoxy recyclerView

--- app/src/main/java/com/example/revolut/currencyrate/CurrencyRateFragment.kt
+++ app/src/main/java/com/example/revolut/currencyrate/CurrencyRateFragment.kt
@@ -4,16 +4,64 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.epoxy.EpoxyRecyclerView
 import com.example.revolut.R
+import com.example.revolut.common.ext.scrollPercentage
+import com.example.revolut.currencyrate.domain.CurrencyRate
+import com.example.revolut.currencyrate.ui.ExchangeRateEpoxyController
 
 class CurrencyRateFragment : Fragment(){
 
     // views
     private lateinit var root: View
+    private lateinit var textRatesTitle: TextView
+    private lateinit var recyclerViewRates: EpoxyRecyclerView
+
+    // variables
+    private val exchangeRateEpoxyController = ExchangeRateEpoxyController()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         root = inflater.inflate(R.layout.fragment_currency_rates, container, false)
+        recyclerViewRates = root.findViewById(R.id.recyclerViewRates)
+        textRatesTitle = root.findViewById(R.id.textRatesTitle)
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        setupRecyclerViewScrollListener()
+    }
+
+    private fun setupRecyclerViewScrollListener() {
+        recyclerViewRates.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                textRatesTitle.elevation = recyclerView.scrollPercentage()
+            }
+        })
+    }
+
+    private fun setupRecyclerView() {
+        recyclerViewRates.adapter = exchangeRateEpoxyController.adapter
+        exchangeRateEpoxyController.setData(getStubData())
+    }
+
+    private fun getStubData() = listOf(
+        CurrencyRate("BGN"),
+        CurrencyRate("BRL"),
+        CurrencyRate("CAD"),
+        CurrencyRate("CHF"),
+        CurrencyRate("BRL"),
+        CurrencyRate("GBP"),
+        CurrencyRate("BGN"),
+        CurrencyRate("BRL"),
+        CurrencyRate("CAD"),
+        CurrencyRate("CHF"),
+        CurrencyRate("BRL"),
+        CurrencyRate("GBP"),
+        CurrencyRate("EUR")
+    )
 }
