d0ce9dfb330de09d15fe31d3253daacac2995226
Matt Taila <mtailacodes@gmail.com>
2019-11-20 04:02:42 +0000

    Created function to return a map of CurrencyCode to CountryCode  - will use this for resource ID's

--- app/src/main/java/com/example/revolut/common/ext/CurrencyRelatedExtension.kt
+++ app/src/main/java/com/example/revolut/common/ext/CurrencyRelatedExtension.kt
@@ -1,5 +1,19 @@
 package com.example.revolut.common.ext
 
+import android.util.Log
 import java.util.*
 
 fun String.getCurrencyDisplayName() = Currency.getInstance(this).displayName
+
+fun mapCurrenciesCodesToCountryCodes(): HashMap<String, String> {
+    val hashMap = HashMap<String, String>()
+    Locale.getAvailableLocales().forEach { locale ->
+        locale.unicodeLocaleAttributes
+        try {
+            hashMap[Currency.getInstance(locale).currencyCode] = locale.country.toLowerCase(locale)
+        } catch (e: Exception) {
+            Log.e("Currency Code error:", e.localizedMessage)
+        }
+    }
+    return hashMap
+}
