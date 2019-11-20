a6e0934a198720cad4cf27668fbdd410f0252061
Matt Taila <mtailacodes@gmail.com>
2019-11-20 04:05:57 +0000

    Refactored data classes

--- app/src/main/java/com/example/revolut/currencyrate/domain/ExchangeRate.kt
+++ app/src/main/java/com/example/revolut/currencyrate/domain/ExchangeRate.kt
@@ -1,25 +1,13 @@
 package com.example.revolut.currencyrate.domain
 
-import kotlin.random.Random
-
 data class ExchangeRate(
     val base: String,
     val date: String,
     val rates: Map<String, Double>
-) {
-    fun toCurrencyRate(): List<CurrencyRate> {
-        val currencyRateList = mutableListOf<CurrencyRate>()
-        currencyRateList.add(CurrencyRate(currencyCode = base))
-        rates.forEach {
-            currencyRateList.add(CurrencyRate(it.key, it.value))
-        }
-        return currencyRateList
-    }
-}
-// todo add function to get resource icons
+)
 
 data class CurrencyRate(
     val currencyCode: String,
-    val conversionRate: Double? = Random.nextDouble(0.1, 19.0),
+    val conversionRate: Double?,
     var currencyAssociatedResourceId: Int = 0
 )
